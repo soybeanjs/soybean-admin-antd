348b4f6310165d8759a4428a588ccf31b15e5bc7
Soybean <soybeanjs@outlook.com>
2024-04-14 12:50:36 +0800

    feat(hooks): @sa/hooks: add useTable

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -3,5 +3,8 @@ import useLoading from './use-loading';
 import useCountDown from './use-count-down';
 import useContext from './use-context';
 import useSvgIconRender from './use-svg-icon-render';
+import useHookTable from './use-table';
 
-export { useBoolean, useLoading, useCountDown, useContext, useSvgIconRender };
+export { useBoolean, useLoading, useCountDown, useContext, useSvgIconRender, useHookTable };
+
+export * from './use-table';
