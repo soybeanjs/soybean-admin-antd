89848496cabaec0ee397b82194e1e9d9f699686b
wang <1509326266@qq.com>
2024-06-06 16:29:03 +0800

    ptimize(components): ButtonIcon with smarter class merging and tippy

--- src/components/custom/button-icon.vue
+++ src/components/custom/button-icon.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { createReusableTemplate } from '@vueuse/core';
 import type { TooltipPlacement } from 'ant-design-vue/es/tooltip';
-
+import { twMerge } from 'tailwind-merge';
 defineOptions({
   name: 'ButtonIcon',
   inheritAttrs: false
@@ -29,59 +27,23 @@ const props = withDefaults(defineProps<Props>(), {
   triggerParent: false
 });
 
-interface ButtonProps {
-  className: string;
-}
-
-const [DefineButton, Button] = createReusableTemplate<ButtonProps>();
-
-const cls = computed(() => {
-  let clsStr = props.class;
-
-  if (!clsStr.includes('h-')) {
-    clsStr += ' h-36px';
-  }
-
-  if (!clsStr.includes('text-')) {
-    clsStr += ' text-icon';
-  }
-
-  return clsStr;
-});
-
 function getPopupContainer(triggerNode: HTMLElement) {
   return props.triggerParent ? triggerNode.parentElement! : document.body;
 }
+
+const DEFAULT_CLASS = 'h-[36px] text-icon';
 </script>
 
 <template>
-  <!-- define component start: Button -->
-  <DefineButton v-slot="{ $slots, className }">
-    <AButton type="text" :class="className">
+  <ATooltip :placement="tooltipPlacement" :get-popup-container="getPopupContainer" :title="tooltipContent">
+    <AButton type="text" :class="twMerge(DEFAULT_CLASS, props.class)" v-bind="$attrs">
       <div class="flex-center gap-8px">
-        <component :is="$slots.default" />
+        <slot>
+          <SvgIcon :icon="icon" />
+        </slot>
       </div>
     </AButton>
-  </DefineButton>
-  <!-- define component end: Button -->
-
-  <ATooltip
-    v-if="tooltipContent"
-    :placement="tooltipPlacement"
-    :get-popup-container="getPopupContainer"
-    :title="tooltipContent"
-  >
-    <Button :class-name="cls" v-bind="$attrs">
-      <slot>
-        <SvgIcon :icon="icon" />
-      </slot>
-    </Button>
   </ATooltip>
-  <Button v-else :class-name="cls" v-bind="$attrs">
-    <slot>
-      <SvgIcon :icon="icon" />
-    </slot>
-  </Button>
 </template>
 
 <style scoped></style>
