cee03ed1a94dfcfbb90e5e84a37c2cf9f0800d8b
Soybean <soybeanjs@outlook.com>
2024-06-07 02:26:18 +0800

    feat(projects): support system new version update notification

---
+++ src/plugins/app.ts
@@ -0,0 +1,55 @@
+import { h } from 'vue';
+import { Button } from 'ant-design-vue';
+import { $t } from '../locales';
+
+export function setupAppVersionNotification() {
+  document.addEventListener('visibilitychange', async () => {
+    const buildTime = await getHtmlBuildTime();
+
+    if (buildTime !== BUILD_TIME && document.visibilityState === 'visible') {
+      const key = `open${Date.now()}`;
+
+      window.$notification?.open({
+        message: $t('system.updateTitle'),
+        description: $t('system.updateContent'),
+        btn() {
+          return h('div', { style: { display: 'flex', justifyContent: 'end', gap: '12px', width: '325px' } }, [
+            h(
+              Button,
+              {
+                onClick() {
+                  window.$notification?.destroy(key);
+                }
+              },
+              $t('system.updateCancel')
+            ),
+            h(
+              Button,
+              {
+                type: 'primary',
+                onClick() {
+                  location.reload();
+                }
+              },
+              $t('system.updateConfirm')
+            )
+          ]);
+        }
+      });
+    }
+  });
+}
+
+async function getHtmlBuildTime() {
+  const baseURL = import.meta.env.VITE_BASE_URL;
+
+  const res = await fetch(`${baseURL}index.html`);
+
+  const html = await res.text();
+
+  const match = html.match(/<meta name="buildTime" content="(.*)">/);
+
+  const buildTime = match?.[1] || '';
+
+  return buildTime;
+}
